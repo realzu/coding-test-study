b076219f3323006664703a1ab5e672c8c17a2db2
진현주 <97022695+realzu@users.noreply.github.com>
2022-06-17 10:56:23 +0900

    [입출력] 6009번 - 6017번

---
+++ code-up/6009-6017.py
@@ -0,0 +1,48 @@
+# 6009
+char = input()
+print(char)
+
+
+# 6010
+val = int(input())
+print(val)
+
+
+# 6011  ★
+val = float(input())    #float()
+print(val)
+
+
+# 6012
+a = int(input())
+b = int(input())
+print(a)
+print(b)
+
+
+# 6013
+a = input()
+b = input()
+print(b + '\n' + a)
+
+
+# 6014
+val = float(input())
+for i in range(3):
+    print(val)
+
+
+# 6015
+a, b = input().split(' ')
+print(int(a))
+print(int(b))
+
+
+# 6016
+a, b = input().split(' ')
+print(b, a)
+
+
+# 6017
+data = input()
+print(data, data, data)
