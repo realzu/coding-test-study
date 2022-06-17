dd3ec337087d4ba95de17d78ca887785eb91db42
진현주 <97022695+realzu@users.noreply.github.com>
2022-06-17 09:51:26 +0900

    [출력] 6001번 - 6008번

---
+++ 6001-6008.py
@@ -0,0 +1,35 @@
+# 6001
+print('Hello')
+
+
+# 6002
+print('Hello World')
+print('Hello', 'World')
+print('Hello, World')   #x
+
+
+# 6003
+print('Hello')
+print('World')
+print('Hello\nWorld')
+
+
+# 6004
+print("'Hello'")
+
+
+# 6005
+print('"Hello World"')
+print("\"Hello World\"")
+
+
+# 6006
+print('"!@#$%^&*()\'')
+
+
+# 6007
+print('"C:\\Download\\\'hello\'.py"')
+
+
+# 6008
+print('print("Hello\\nWorld")')
