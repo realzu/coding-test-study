79357ad1fbfe870714e99fda2446c9e242db7f3b
진현주 <97022695+realzu@users.noreply.github.com>
2022-06-17 12:32:51 +0900

    [입출력] 6018번 - 6024번

---
+++ code-up/6018-6024.py
@@ -0,0 +1,38 @@
+# # 6018
+# hour, minute = input().split(':')
+# print('{0}:{1}'.format(hour, minute))
+# # print(hour, minute, sep=':')    #★sep
+
+
+# # 6019
+# y, m, d = input().split('.')
+# print(d, m, y, sep='-')
+
+
+# # 6020
+# a, b = input().split('-')
+# print(a + b)
+# # print(a, b, sep='')
+
+
+# 6021  ★
+# val = input()
+
+# for i in range(5):
+#     print(val[i])   #배열 정의 안줘도 되네
+
+
+# # 6022  
+# val = input()
+# print(val[0]+val[1], val[2]+val[3], val[4]+val[5], sep=' ')
+# # ★ s[a:b] -> a부터 b-1번째 문자열
+
+
+# # 6023
+# h, m, s = input().split(':')
+# print(m)
+
+
+# 6024
+a, n =input().split()   #split()은 공백(스페이스, 엔터, 탭 등) 기준
+print(a + n)
