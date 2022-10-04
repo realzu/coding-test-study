7e0dcf1a572408e59b78524fb34584fedb5cd67b
진현주 <97022695+realzu@users.noreply.github.com>
2022-10-04 23:03:53 +0900

    section8 : 6

---
+++ Inflearn/8.재귀함수와 완전탐색/6_바둑이 승차.js
@@ -0,0 +1,20 @@
+// @
+function solution(c, arr){
+    let answer=Number.MIN_SAFE_INTEGER;
+    let n=arr.length;
+    function DFS(L, sum){
+        if(sum>c) return;
+        if(L===n){
+            answer=Math.max(answer, sum);
+        }
+        else{
+            DFS(L+1, sum+arr[L]);
+            DFS(L+1, sum);
+        }                  
+    }
+    DFS(0, 0);
+    return answer;
+}
+
+let arr=[81, 58, 42, 33, 61];
+console.log(solution(259, arr));
