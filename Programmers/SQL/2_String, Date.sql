c0280c9caacb03009ca42664e113b754ce476103
진현주 <97022695+realzu@users.noreply.github.com>
2022-11-05 13:18:27 +0900

    SQL Kit : 13 ~ 17

---
+++ Programmers/SQL/2_String, Date.sql
@@ -0,0 +1,12 @@
+-- 루시와 엘라 찾기
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID
+
+-- 이름에 el이 들어가는 동물 찾기
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE UPPER(NAME) LIKE '%EL%'
+AND ANIMAL_TYPE = 'Dog'
+ORDER BY NAME
