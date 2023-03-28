06f48c7d7b0139f0ccc16b32ec22e896ccf1b221
realzu <jinhh501@naver.com>
2023-03-28 16:05:07 +0900

    정렬: 7-7

---
+++ Inflearn/Study/7-7_좌표 정렬.js
@@ -0,0 +1,11 @@
+function solution(arr){
+    arr.sort((a, b) => {
+        if (a[0] === b[0]) return a[1] - b[1];
+        return a[0] - b[0];
+    })
+
+    return arr;
+}
+
+let arr=[[2, 7], [1, 3], [1, 2], [2, 5], [3, 6]];
+console.log(solution(arr));
